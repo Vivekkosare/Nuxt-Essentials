4ecd9dfd0978991e6c2251d036e8b58ab04ec4d2
Vivek Kosare <v_kosare@yahoo.com>
2020-07-11 23:05:49 +0200

    added folder structure in nuxt app

---
+++ pages/products/_id/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+      <h3>this is my id: {{this.$route.params.id}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+      <h1>welcome to products</h1>
+     <nuxt-link to="/products/1">product 1</nuxt-link>
+     <nuxt-link to="/products/2">product 2</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
