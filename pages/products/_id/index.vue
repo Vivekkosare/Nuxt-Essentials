45df237cdc67859b8cb9b0a578418865dcd2eb2c
Vivek Kosare <v_kosare@yahoo.com>
2020-07-12 00:55:47 +0200

    added folder structure

--- pages/products/_id/index.vue
+++ pages/products/_id/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-      <h3>this is my id: {{this.$route.params.id}}</h3>
+      <h3>this is the product id: {{this.$route.params.id }}</h3>
   </div>
 </template>
 
